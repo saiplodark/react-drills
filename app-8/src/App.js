e48657cd7f4e74033b80632029b66c9d7c7af9a4
Cory iMac27 MD17 <cory@Cory-iMac27-MD17.local>
2020-05-10 23:58:18 -0700

    8 swapi died

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemon: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://pokeapi.co/api/v2/pokemon/1/").then(response => {
+      this.setState({
+        pokemon: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name: {this.state.pokemon.name}</h1>
+        <h1>Base experience: {this.state.pokemon.base_experience}</h1>
+        <h1>Height: {this.state.pokemon.height}</h1>
+        <h1>Order: {this.state.pokemon.order}</h1>
+        <h1>Weight: {this.state.pokemon.weight}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
